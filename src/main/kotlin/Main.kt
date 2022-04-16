62eecd3bce065c83a51a308bf57ec0af423d5617
DasLixou <einlixou@gmail.com>
2022-04-16 14:08:40 +0200

    🌱 Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+import cc.lixou.stracciatella.Stracciatella
+
+fun main(args: Array<String>) {
+    Stracciatella().start()
+}
---
+++ src/main/kotlin/cc/lixou/stracciatella/Stracciatella.kt
@@ -0,0 +1,33 @@
+package cc.lixou.stracciatella
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.event.player.PlayerLoginEvent
+import net.minestom.server.instance.block.Block
+
+class Stracciatella {
+
+    fun start() {
+        val server = MinecraftServer.init()
+
+        val instanceManager = MinecraftServer.getInstanceManager()
+
+        val instanceContainer = instanceManager.createInstanceContainer()
+
+        instanceContainer.setGenerator { unit ->
+            unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
+        }
+
+        val eventHandler = MinecraftServer.getGlobalEventHandler()
+        eventHandler.addListener(PlayerLoginEvent::class.java) { event ->
+            val player = event.player
+            event.setSpawningInstance(instanceContainer)
+            player.respawnPoint = Pos(0.0, 42.0, 0.0)
+        }
+
+        MinecraftServer.setBrandName("Stracciatella (Minestom powered)")
+
+        server.start("0.0.0.0", 25565)
+    }
+
+}
