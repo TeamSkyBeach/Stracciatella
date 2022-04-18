cf11f64515ffcb5618826bfb3e0914bf2ded69d7
DasLixou <einlixou@gmail.com>
2022-04-18 18:01:21 +0200

    📦 Added AbstractInventoryExtension for styledBackground

---
+++ src/main/kotlin/cc/lixou/stracciatella/inventory/extensions/AbstractInventoryExtension.kt
@@ -0,0 +1,13 @@
+package cc.lixou.stracciatella.inventory.extensions
+
+import net.minestom.server.inventory.AbstractInventory
+import net.minestom.server.item.Material
+
+/**
+ * Styles the inventory
+ * @param rowLength Length of a single row
+ * @param material  the material of the Corner Frames
+ */
+fun AbstractInventory.styledBackground(rowLength: Byte, material: Material) {
+
+}
