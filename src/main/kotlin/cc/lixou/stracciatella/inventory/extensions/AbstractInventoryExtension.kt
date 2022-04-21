e7f92f30fca8c7621a6b4630836088dbca6422c3
DasLixou <einlixou@gmail.com>
2022-04-21 18:36:49 +0200

    📦 Renewed Method Params

--- src/main/kotlin/cc/lixou/stracciatella/inventory/extensions/AbstractInventoryExtension.kt
+++ src/main/kotlin/cc/lixou/stracciatella/inventory/extensions/AbstractInventoryExtension.kt
@@ -5,9 +5,11 @@ import net.minestom.server.item.Material
 
 /**
  * Styles the inventory
- * @param rowLength Length of a single row
- * @param material  the material of the Corner Frames
+ * @param material the material of the corner frames
+ * @param rowAmount amount of rows
+ * @param startPos the index from which to start
+ * @param rowLength length of a single row
  */
-fun AbstractInventory.styledBackground(rowLength: Byte, material: Material) {
+fun AbstractInventory.styleRadialBackground(material: Material, rowAmount: Byte, startPos: Byte = 0, rowLength: Byte = 9) {
 
 }
