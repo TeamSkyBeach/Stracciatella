f0ce8e266f3e9aabe7104d8d3c6464bee780a8c0
DasLixou <einlixou@gmail.com>
2022-04-21 18:14:15 +0200

    🐞 Removed Debug Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,5 @@
 import cc.lixou.stracciatella.Stracciatella
-import cc.lixou.stracciatella.config.Config
-import net.minestom.server.coordinate.Pos
-import world.cepi.kstom.serializer.PositionSerializer
-import java.nio.file.Path
 
 fun main(args: Array<String>) {
-    val data = Test()
-    Config.writeConfig(Path.of("kaka.yml"), data)
-//Stracciatella().start()
-}
-
-@kotlinx.serialization.Serializable
-class Test {
-    @kotlinx.serialization.Serializable(PositionSerializer::class)
-    val pos = Pos(40.0, 12.0, 34.0)
-    @kotlinx.serialization.Serializable
-    val name = "DIETER"
+    Stracciatella().start()
 }
