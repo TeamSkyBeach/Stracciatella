431264d96b8c5887d00dcca4a82554de370e3034
DasLixou <einlixou@gmail.com>
2022-04-21 18:09:21 +0200

    📜 There was just an Library 🥺

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 import cc.lixou.stracciatella.Stracciatella
-import cc.lixou.stracciatella.config.YamlConfiguration
+import cc.lixou.stracciatella.config.Config
+import cc.lixou.stracciatella.utils.serializer.PositionSerializer
+import net.minestom.server.coordinate.Pos
+import java.nio.file.Path
 
 fun main(args: Array<String>) {
-    val config = YamlConfiguration()
-    config.load("test.yml")
-    println(config.data().getValues())
+    val data = Test()
+    Config.writeConfig(Path.of("kaka.yml"), data)
 //Stracciatella().start()
+}
+
+@kotlinx.serialization.Serializable
+class Test {
+    @kotlinx.serialization.Serializable(PositionSerializer::class)
+    val pos = Pos(40.0, 12.0, 34.0)
+    @kotlinx.serialization.Serializable
+    val name = "DIETER"
 }
