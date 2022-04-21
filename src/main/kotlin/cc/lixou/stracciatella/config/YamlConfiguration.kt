a831800107788e96fbe8d6ebe6bde8a12846fe05
DasLixou <einlixou@gmail.com>
2022-04-21 16:56:09 +0200

    📜 Support for Saving

--- src/main/kotlin/cc/lixou/stracciatella/config/YamlConfiguration.kt
+++ src/main/kotlin/cc/lixou/stracciatella/config/YamlConfiguration.kt
@@ -1,16 +1,32 @@
 package cc.lixou.stracciatella.config
 
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
 import java.io.File
+import java.io.FileWriter
 
 class YamlConfiguration : Configuration() {
+
+    companion object {
+        private val yamlOptions = DumperOptions()
+
+        init {
+            yamlOptions.defaultFlowStyle = DumperOptions.FlowStyle.BLOCK
+        }
+
+        private val yaml = Yaml(yamlOptions)
+    }
 
     override fun load(file: File) {
         TODO("Not yet implemented")
     }
 
     override fun save(file: File) {
-        println(data.getValues().toString())
-        TODO("Not yet implemented")
+        val writer = FileWriter(file)
+        val value = yaml.dump(data.getValues())
+        writer.write(value)
+        writer.flush()
+        writer.close()
     }
 
 }
