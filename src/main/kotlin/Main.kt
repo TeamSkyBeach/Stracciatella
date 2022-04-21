522c6f65ebf947232617d6412c5556ed44725212
DasLixou <einlixou@gmail.com>
2022-04-21 16:56:38 +0200

    📜 Testing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
 import cc.lixou.stracciatella.Stracciatella
 import cc.lixou.stracciatella.config.YamlConfiguration
-import java.io.File
 
 fun main(args: Array<String>) {
     val config = YamlConfiguration()
     config.data().set("users.lixou.age", 13)
-    config.save(File("aa.yml"))
+    config.save("test.yml")
 //Stracciatella().start()
 }
