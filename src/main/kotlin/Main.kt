b8cfb973ef8cbbdd3148adfc31b150cd7ef2d131
DasLixou <einlixou@gmail.com>
2022-04-21 15:19:27 +0200

    📜 Main debug stuff

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 import cc.lixou.stracciatella.Stracciatella
+import cc.lixou.stracciatella.config.YamlConfiguration
+import java.io.File
 
 fun main(args: Array<String>) {
-    Stracciatella().start()
+    val config = YamlConfiguration()
+    config.data().set("users.lixou.age", 13)
+    config.save(File("aa.yml"))
+//Stracciatella().start()
 }
