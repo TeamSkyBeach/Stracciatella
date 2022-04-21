d86eb1a792a9cc2d9df52ad5e3f50cc0930566ff
DasLixou <einlixou@gmail.com>
2022-04-21 16:55:45 +0200

    📜 Loading / Saving with fileName

--- src/main/kotlin/cc/lixou/stracciatella/config/Configuration.kt
+++ src/main/kotlin/cc/lixou/stracciatella/config/Configuration.kt
@@ -6,7 +6,13 @@ sealed class Configuration {
 
     protected val data: ConfigurationData = ConfigurationData()
 
+    fun load(fileName: String) {
+        load(File(fileName))
+    }
     abstract fun load(file: File)
+    fun save(fileName: String) {
+        save(File(fileName))
+    }
     abstract fun save(file: File)
 
     fun data(): ConfigurationData = data
