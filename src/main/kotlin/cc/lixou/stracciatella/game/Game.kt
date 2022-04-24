8716a107327179782b3b3e0b68be2b01857df52f
DasLixou <einlixou@gmail.com>
2022-04-24 09:07:54 +0200

    🕹️ canJoin gets multiple Players

--- src/main/kotlin/cc/lixou/stracciatella/game/Game.kt
+++ src/main/kotlin/cc/lixou/stracciatella/game/Game.kt
@@ -1,10 +1,13 @@
 package cc.lixou.stracciatella.game
 
+import net.minestom.server.entity.Player
+
 abstract class Game {
 
     /**
+     * @param players the players that should join
      * @return false if game is full, then creates new instance
      */
-    abstract fun canJoin(): Boolean
+    abstract fun canJoin(players: Array<Player>): Boolean
 
 }
