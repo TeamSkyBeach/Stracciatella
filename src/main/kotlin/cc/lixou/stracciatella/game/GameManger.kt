600f439c8655f74ca875ea1b28359d485104ec4a
DasLixou <einlixou@gmail.com>
2022-04-24 09:22:49 +0200

    🕹️ added Game Registering

--- src/main/kotlin/cc/lixou/stracciatella/game/GameManger.kt
+++ src/main/kotlin/cc/lixou/stracciatella/game/GameManger.kt
@@ -1,3 +1,17 @@
 package cc.lixou.stracciatella.game
 
-object GameManger
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KClass
+
+object GameManger {
+
+    private val games: ConcurrentHashMap<KClass<out Game>, ArrayList<out Game>> = ConcurrentHashMap()
+
+    inline fun <reified T : Game> registerGame() = registerGame(T::class)
+
+    fun <T : Game> registerGame(clazz: KClass<out T>) {
+        if (games.containsKey(clazz)) return
+        games[clazz] = ArrayList()
+    }
+
+}
