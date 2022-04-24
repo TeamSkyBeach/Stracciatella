1d34d502884c03dd0f86c3284bece65a437edee5
DasLixou <einlixou@gmail.com>
2022-04-24 09:35:21 +0200

    🕹️ added joinGame

--- src/main/kotlin/cc/lixou/stracciatella/game/GameManger.kt
+++ src/main/kotlin/cc/lixou/stracciatella/game/GameManger.kt
@@ -1,11 +1,29 @@
 package cc.lixou.stracciatella.game
 
+import net.minestom.server.entity.Player
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
 
 object GameManger {
 
     private val games: ConcurrentHashMap<KClass<out Game>, ArrayList<out Game>> = ConcurrentHashMap()
+
+    inline fun <reified T : Game> joinGame(players: Array<Player>) = joinGame(T::class, players)
+
+    fun <T : Game> joinGame(clazz: KClass<out T>, players: Array<Player>) {
+        val currentGames = games[clazz]
+        var game: Game? = null
+        currentGames?.forEach {
+            if (it.canJoin(players)) {
+                game = it
+                return@forEach
+            }
+        }
+        if (game == null) {
+            // TODO: Create new Game
+        }
+        // TODO: Make Game joinable
+    }
 
     inline fun <reified T : Game> registerGame() = registerGame(T::class)
 
