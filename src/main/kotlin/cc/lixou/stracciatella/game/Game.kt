23f9fe1559fa3451f7cdae9e33a8935b352535d8
DasLixou <einlixou@gmail.com>
2022-04-24 14:42:28 +0200

    🕹️ added instances, uuid and eventNode

--- src/main/kotlin/cc/lixou/stracciatella/game/Game.kt
+++ src/main/kotlin/cc/lixou/stracciatella/game/Game.kt
@@ -1,10 +1,30 @@
 package cc.lixou.stracciatella.game
 
 import net.minestom.server.entity.Player
+import net.minestom.server.event.EventFilter
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.trait.PlayerEvent
+import net.minestom.server.instance.Instance
+import world.cepi.kstom.Manager
+import java.util.*
 
 abstract class Game {
 
     protected val players = ArrayList<Player>()
+    protected val instances = ArrayList<Instance>()
+    protected val uuid: UUID = UUID.randomUUID()
+    protected val eventNode =
+        EventNode.type("${this.javaClass.simpleName}-${uuid}", EventFilter.INSTANCE) { event, instance ->
+            if (event is PlayerEvent) {
+                return@type players.contains(event.player)
+            } else {
+                return@type instances.firstOrNull { it.uniqueId == instance.uniqueId } != null
+            }
+        }
+
+    init {
+        Manager.globalEvent.addChild(eventNode)
+    }
 
     /**
      * @param players the players that should join
