567f1164d258c19a300b429117df50bf6f53b753
DasLixou <einlixou@gmail.com>
2022-04-27 18:56:09 +0200

    🗺️ storing sectiondata from sections

--- src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedChunkData.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedChunkData.kt
@@ -11,7 +11,7 @@ class IcedChunkData(
 
         fun load(dis: DataInputStream): IcedChunkData {
             val sections = mutableListOf<IcedSectionData>()
-            for (chunkSection in 0 until 15) {
+            for (chunkSection in 0 until 16) {
                 sections.add(chunkSection, IcedSectionData.load(dis))
             }
 
@@ -20,6 +20,9 @@ class IcedChunkData(
 
         fun fromChunk(chunk: Chunk): IcedChunkData {
             val sections = mutableListOf<IcedSectionData>()
+            for (chunkSection in 0 until 16) {
+                sections.add(chunkSection, IcedSectionData.fromChunk(chunk, chunk.getSection(chunkSection)))
+            }
 
             return IcedChunkData(sections.toTypedArray())
         }
--- src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedSectionData.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedSectionData.kt
@@ -1,6 +1,8 @@
 package cc.lixou.stracciatella.instance.data
 
 import cc.lixou.stracciatella.instance.util.NBTUtils
+import net.minestom.server.instance.Chunk
+import net.minestom.server.instance.Section
 import net.minestom.server.instance.batch.RelativeBlockBatch
 import org.jglrxavpok.hephaistos.collections.ImmutableLongArray
 import org.jglrxavpok.hephaistos.mca.unpack
@@ -59,6 +61,24 @@ class IcedSectionData(
             val skyLight = if (hasSkyLight) {
                 dis.readNBytes(2048)
             } else ByteArray(2048)
+
+            return IcedSectionData(batch, blockLight, skyLight)
+        }
+
+        fun fromChunk(chunk: Chunk, section: Section): IcedSectionData {
+            val blockLight = section.blockLight
+
+            val batch = RelativeBlockBatch()
+
+            for (y in 0 until 16) {
+                for (z in 0 until 16) {
+                    for (x in 0 until 16) {
+                        batch.setBlock(x, y, z, chunk.getBlock(x, y, z))
+                    }
+                }
+            }
+
+            val skyLight = section.skyLight
 
             return IcedSectionData(batch, blockLight, skyLight)
         }
