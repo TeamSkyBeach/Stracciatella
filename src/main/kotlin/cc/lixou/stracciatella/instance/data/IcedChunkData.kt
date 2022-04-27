9e2a8ed46830a2352f82c043eabb02b288a41822
DasLixou <einlixou@gmail.com>
2022-04-27 18:47:37 +0200

    🗺️ added fromChunks Methods

--- src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedChunkData.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedChunkData.kt
@@ -1,5 +1,6 @@
 package cc.lixou.stracciatella.instance.data
 
+import net.minestom.server.instance.Chunk
 import java.io.DataInputStream
 
 class IcedChunkData(
@@ -13,6 +14,12 @@ class IcedChunkData(
             for (chunkSection in 0 until 15) {
                 sections.add(chunkSection, IcedSectionData.load(dis))
             }
+
+            return IcedChunkData(sections.toTypedArray())
+        }
+
+        fun fromChunk(chunk: Chunk): IcedChunkData {
+            val sections = mutableListOf<IcedSectionData>()
 
             return IcedChunkData(sections.toTypedArray())
         }
--- src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedObjectData.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedObjectData.kt
@@ -1,5 +1,6 @@
 package cc.lixou.stracciatella.instance.data
 
+import net.minestom.server.instance.Chunk
 import java.io.DataInputStream
 import java.io.DataOutputStream
 
@@ -25,6 +26,17 @@ class IcedObjectData(
             return IcedObjectData(sizeX, sizeZ, chunkData)
         }
 
+        fun fromChunks(chunks: List<Chunk>): IcedObjectData {
+            val sizeX = chunks.maxOf { it.chunkX } - chunks.minOf { it.chunkX } + 1
+            val sizeZ = chunks.maxOf { it.chunkZ } - chunks.minOf { it.chunkZ } + 1
+
+            val chunkData = mutableListOf<IcedChunkData>()
+            for (chunk in chunks) {
+                chunkData.add(IcedChunkData.fromChunk(chunk))
+            }
+
+            return IcedObjectData(sizeX.toUShort(), sizeZ.toUShort(), chunkData)
+        }
     }
 
     fun save(dos: DataOutputStream) {
