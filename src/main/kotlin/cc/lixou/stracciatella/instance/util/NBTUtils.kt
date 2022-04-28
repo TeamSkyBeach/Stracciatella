2377b05bd9525a2f0c7917db160f1f457641c325
DasLixou <einlixou@gmail.com>
2022-04-28 20:43:34 +0200

    🗺️ make compound out of block

--- src/main/kotlin/cc/lixou/stracciatella/instance/util/NBTUtils.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/util/NBTUtils.kt
@@ -2,6 +2,7 @@ package cc.lixou.stracciatella.instance.util
 
 import net.minestom.server.instance.block.Block
 import org.jglrxavpok.hephaistos.nbt.*
+import org.jglrxavpok.hephaistos.nbt.mutable.MutableNBTCompound
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
@@ -27,6 +28,17 @@ object NBTUtils {
         }
 
         return Block.fromNamespaceId(name)?.withProperties(newProps)
+    }
+
+    fun getCompoundFromBlock(block: Block): NBTCompound {
+        val compund = MutableNBTCompound()
+        compund.setString("Name", block.name())
+        val properties = MutableNBTCompound()
+        for (property in block.properties()) {
+            properties.setString(property.key, property.value)
+        }
+        compund["Properties"] = properties.toCompound()
+        return compund.toCompound()
     }
 
 }
