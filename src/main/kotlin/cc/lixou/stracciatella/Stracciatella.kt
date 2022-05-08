195a944713b01caab2167fbc83619768d8c632f2
DasLixou <einlixou@gmail.com>
2022-05-08 13:04:46 +0200

    🛫 Seperated Stracciatella start

--- src/main/kotlin/cc/lixou/stracciatella/Stracciatella.kt
+++ src/main/kotlin/cc/lixou/stracciatella/Stracciatella.kt
@@ -9,9 +9,15 @@ import net.minestom.server.instance.block.Block
 
 class Stracciatella {
 
-    fun start() {
-        val server = MinecraftServer.init()
+    private val server = MinecraftServer.init()
 
+    init {
+        val eventHandler = MinecraftServer.getGlobalEventHandler()
+        eventHandler.addListener(PlayerDisconnectEvent::class.java) { event ->
+            GameManager.unregisterPlayer(event.player)
+        }
+
+        // region [Test World Debug Stuff - REMOVE]
         val instanceManager = MinecraftServer.getInstanceManager()
 
         val instanceContainer = instanceManager.createInstanceContainer()
@@ -19,19 +25,17 @@ class Stracciatella {
         instanceContainer.setGenerator { unit ->
             unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
         }
-
-        val eventHandler = MinecraftServer.getGlobalEventHandler()
-        eventHandler.addListener(PlayerDisconnectEvent::class.java) { event ->
-            GameManager.unregisterPlayer(event.player)
-        }
         eventHandler.addListener(PlayerLoginEvent::class.java) { event ->
             val player = event.player
             event.setSpawningInstance(instanceContainer)
             player.respawnPoint = Pos(0.0, 42.0, 0.0)
         }
+        // endregion
 
         MinecraftServer.setBrandName("Stracciatella (Minestom powered)")
+    }
 
+    fun start() {
         server.start("0.0.0.0", 25565)
     }
 
