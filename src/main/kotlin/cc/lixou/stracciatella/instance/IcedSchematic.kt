f733aa93626859f2b1e88c47788a8ddb42a62270
DasLixou <einlixou@gmail.com>
2022-05-22 16:31:03 +0200

    ⚙️ Now using GZIP

--- src/main/kotlin/cc/lixou/stracciatella/instance/IcedSchematic.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/IcedSchematic.kt
@@ -2,6 +2,8 @@ package cc.lixou.stracciatella.instance
 
 import cc.lixou.stracciatella.instance.data.IcedObjectData
 import java.io.*
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 object IcedSchematic {
 
@@ -9,7 +11,8 @@ object IcedSchematic {
         FileInputStream(file).let { val value = fromInputStream(it); it.close(); value }
 
     fun fromInputStream(inputStream: InputStream): Map<String, IcedObjectData> {
-        val reader = DataInputStream(inputStream)
+        val gzip = GZIPInputStream(inputStream)
+        val reader = DataInputStream(gzip)
         val objects = mutableMapOf<String, IcedObjectData>()
         val objectAmount = reader.readByte()
 
@@ -20,8 +23,6 @@ object IcedSchematic {
             objects[name] = objectData
         }
 
-        println(objectAmount)
-
         reader.close()
 
         return objects
@@ -31,7 +32,8 @@ object IcedSchematic {
         FileOutputStream(file).also { toOututStream(it, objectData); it.flush(); it.close() }
 
     fun toOututStream(outptStream: OutputStream, objectData: Map<String, IcedObjectData>) {
-        val writer = DataOutputStream(outptStream)
+        val gzip = GZIPOutputStream(outptStream)
+        val writer = DataOutputStream(gzip)
 
         writer.writeByte(objectData.size)
 
