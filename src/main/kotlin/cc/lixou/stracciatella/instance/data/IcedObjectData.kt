4755bc260cb8c4b85777b25bbdaa4f83f3108e9a
DasLixou <einlixou@gmail.com>
2022-06-02 16:43:32 +0200

    🗺️ Fixed Saving

--- src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedObjectData.kt
+++ src/main/kotlin/cc/lixou/stracciatella/instance/data/IcedObjectData.kt
@@ -39,12 +39,15 @@ class IcedObjectData(
         }
 
         fun fromChunks(chunks: List<Chunk>): IcedObjectData {
-            val sizeX = chunks.maxOf { it.chunkX } - chunks.minOf { it.chunkX } + 1
-            val sizeZ = chunks.maxOf { it.chunkZ } - chunks.minOf { it.chunkZ } + 1
+            val minX = chunks.minOf { it.chunkX }
+            val minZ = chunks.minOf { it.chunkZ }
+            val sizeX = chunks.maxOf { it.chunkX } - minX + 1
+            val sizeZ = chunks.maxOf { it.chunkZ } - minZ + 1
 
             val chunkData = mutableMapOf<Pair<Short, Short>, IcedChunkData>()
             for (chunk in chunks) {
-                chunkData[Pair(chunk.chunkX.toShort(), chunk.chunkZ.toShort())] = IcedChunkData.fromChunk(chunk)
+                chunkData[Pair((chunk.chunkX - minX).toShort(), (chunk.chunkZ - minZ).toShort())] =
+                    IcedChunkData.fromChunk(chunk)
             }
 
             return IcedObjectData(sizeX.toUShort(), sizeZ.toUShort(), chunkData)
@@ -55,8 +58,10 @@ class IcedObjectData(
         dos.writeShort(sizeX.toInt())
         dos.writeShort(sizeZ.toInt())
 
+        println(chunkData)
         for (x in 0 until sizeX.toInt()) {
             for (z in 0 until sizeZ.toInt()) {
+                println("SAVING $x $z")
                 chunkData[Pair(x.toShort(), z.toShort())]?.save(dos)
             }
         }
